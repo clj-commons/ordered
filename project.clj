40b8dc0adedf9e040dfeb1d7cec7a42b0ae059d9
Alan Malloy <alan@malloys.org>
2011-06-18 20:38:00 -0700

    Call this version 0.1.0, I guess

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject ordered "0.0.1-SNAPSHOT"
-  :description "TODO: add summary of your project"
+(defproject ordered "0.1.0"
+  :description "Pure-clojure implementation of ruby's ordered hash type: instead of sorting by key, these maps retain insertion order."
   :dependencies [[clojure "1.2.1"]
                  [clojure-contrib "1.2.0"]
                  [amalloy/utils "[0.3.7,)"]])
