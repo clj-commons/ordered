16d6771dc4bad5eac6e6ab3a006b417719b90b62
Alan Malloy <alan@malloys.org>
2011-06-18 23:10:29 -0700

    Add transient support for sets, and fix implementation of some set methods

--- src/ordered/set.clj
+++ src/ordered/set.clj
@@ -2,7 +2,7 @@
   (:use [deftype.delegate :only [delegating-deftype]]
         [ordered.map :only [ordered-map]])
   (:import (clojure.lang IPersistentSet IObj IEditableCollection
-                         SeqIterator Reversible)
+                         SeqIterator Reversible ITransientSet)
            (java.util Set Collection)
            ordered.map.OrderedMap))
 
@@ -10,9 +10,7 @@
 
 (delegating-deftype OrderedSet [^OrderedMap backing-map]
   {backing-map {IPersistentSet [(get [k])
-                                (count [])]
-                Collection [(size [])
-                            (isEmpty [])]}}
+                                (count [])]}}
 
   IPersistentSet
   (disjoin [this k]
@@ -21,14 +19,32 @@
         (OrderedSet. (.assoc backing-map k k)))
   (seq [this]
        (seq (keys backing-map)))
+  (empty [this]
+         (OrderedSet. (ordered-map)))
+  (equiv [this other]
+         (.equals this other))
+                
+  Object
+  (hashCode [this]
+            (reduce + (map hash (.seq this))))
+  (equals [this other]
+          (or (identical? this other)
+              (and (instance? Set other)
+                   (let [^Set s (cast Set other)]
+                     (and (= (.size this) (.size s))
+                          (every? #(.contains s %) this))))))
 
-  Collection
+  Set
   (iterator [this]
             (SeqIterator. (.seq this)))
   (contains [this k]
             (.containsKey backing-map k))
   (containsAll [this ks]
                (every? identity (map #(.contains this %) ks)))
+  (size [this]
+        (.count this))
+  (isEmpty [this]
+           (zero? (.count this)))
   (toArray [this dest]
            (reduce (fn [idx item]
                      (aset dest idx item)
@@ -40,6 +56,34 @@
 
   Reversible
   (rseq [this]
-        (seq (map key (rseq backing-map)))))
+        (seq (map key (rseq backing-map))))
 
+  IEditableCollection
+  (asTransient [this]
+               (transient-ordered-set this)))
 
+(def ^{:private true,
+       :tag OrderedSet} empty-ordered-set (empty (OrderedSet. nil)))
+
+(defn ordered-set
+  ([] empty-ordered-set)
+  ([coll] (into empty-ordered-set coll)))
+
+(delegating-deftype TransientOrderedSet [^{:unsynchronized-mutable true} backing-map]
+  {backing-map {ITransientSet [(count [])
+                               (get [k])]}}
+
+  ITransientSet
+  (disjoin [this k]
+           (set! backing-map (disj! backing-map k))
+           this)
+  (conj [this k]
+        (set! backing-map (assoc! backing-map k k))
+        this)
+  (contains [this k]
+            (.containsKey backing-map k))
+  (persistent [this]
+              (OrderedSet. (persistent! backing-map))))
+
+(defn transient-ordered-set [^OrderedSet os]
+  (TransientOrderedSet. (transient (.backing-map os))))
