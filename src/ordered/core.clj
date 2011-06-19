6880841f195fb98e220646868f5b4513e84c6cca
Alan Malloy <alan@malloys.org>
2011-06-18 20:02:06 -0700

    Add transient support

--- src/ordered/core.clj
+++ src/ordered/core.clj
@@ -1,13 +1,15 @@
 (ns ordered.core
   (:import (clojure.lang IPersistentMap
-                         IPersistentCollection
                          IPersistentVector
+                         IEditableCollection
+                         ITransientMap
                          IObj
                          IFn
                          MapEquivalence
                          Counted
                          Associative
                          Reversible
+                         ILookup
                          ;; Indexed, maybe add later?
                          ;; Sorted almost certainly not accurate
                          )
@@ -71,10 +73,12 @@ options defined for it, delegating-deftype may break with them."
       `(deftype ~cname [~@fields]
          ~@(explode (aggregate (apply concat deftype-args our-stuff)))))))
 
+(declare transient-ordered-map)
+
 (delegating-deftype OrderedMap [^IPersistentMap backing-map
                                 ^IPersistentVector key-order]
   {backing-map {Counted [(count [])]
-                IPersistentCollection [(equiv [other])]
+                IPersistentMap [(equiv [other])]
                 Associative [(entryAt [k])
                              (valAt [k])
                              (valAt [k not-found])]
@@ -111,6 +115,10 @@ options defined for it, delegating-deftype may break with them."
   (iterator [this]
             (clojure.lang.SeqIterator. (seq this)))
 
+  IEditableCollection
+  (asTransient [this]
+               (transient-ordered-map this))
+
   Reversible
   (rseq [this]
         (seq (OrderedMap. backing-map (rseq key-order)))))
@@ -121,6 +129,56 @@ options defined for it, delegating-deftype may break with them."
 (defn ordered-map
   ([] empty-ordered-map)
   ([coll]
-     (into empty-ordered-map coll))
+     (if (map? coll)
+       (OrderedMap. coll (vec (keys coll))) ;; should be faster
+       (into empty-ordered-map coll)))
   ([k v & more]
      (apply assoc empty-ordered-map k v more)))
+
+;; contains? is broken for transients. we could define a closure around a gensym
+;; to use as the not-found argument to a get, but deftype can't be a closure.
+;; instead, we pass `this` as the not-found argument and hope nobody makes a
+;; transient contain itself.
+(delegating-deftype TransientOrderedMap [^{:unsynchronized-mutable true} backing-map
+                                         ^{:unsynchronized-mutable true} key-order
+                                         ^{:unsynchronized-mutable true} dissocs]
+  {backing-map {Counted [(count [])]
+                ILookup [(valAt [k])
+                         (valAt [k not-found])]}}
+  
+  ITransientMap
+  (assoc [this k v]
+    (when (identical? this (get backing-map k this))
+      ;; not-found returned, so it wasn't already there
+      (set! key-order (conj! key-order k)))
+    (set! backing-map (assoc! backing-map k v))
+    this)
+  
+  (without [this k]
+           (when-not (identical? this (get backing-map k this))
+             ;; not-found not returned, so it was already there
+             (set! backing-map (dissoc! backing-map k))
+             ;; defer updating key-order until persistence, since it's expensive
+             (set! dissocs (conj! dissocs k)))
+           this)
+  
+  (persistent [this]
+              (OrderedMap. (persistent! backing-map)
+                           (let [key-order (persistent! key-order)
+                                 dissocs (seq (persistent! dissocs))]
+                             (if-not dissocs
+                               key-order
+                               (vec (reduce (fn [order dissoc]
+                                              (let [[before [x & after]]
+                                                    (split-with #(not= % dissoc)
+                                                                order)]
+                                                (concat before after)))
+                                            key-order, dissocs))))))
+  (conj [this e]
+        (let [[k v] e]
+          (assoc! this k v))))
+
+(defn transient-ordered-map [^OrderedMap om]
+  (TransientOrderedMap. (transient (.backing-map om))
+                        (transient (.key-order om))
+                        (transient [])))
