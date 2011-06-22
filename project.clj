4716dba6b0b8ec3c0c2270a37af875ab05f1da07
Alan Malloy <alan@malloys.org>
2011-06-22 13:02:27 -0700

    Bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ordered "0.2.0"
+(defproject ordered "0.3.0"
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
   :dependencies [[clojure "1.2.1"]
                  [clojure-contrib "1.2.0"]
