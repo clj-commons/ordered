d544f01d6b0e4e74540263a7ceaca16f0dfdea74
Anthony Grimes <i@raynes.me>
2011-07-19 18:10:37 -0500

    Added cake-marginalia.

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
   :dependencies [[clojure "1.2.1"]
                  [clojure-contrib "1.2.0"]
-                 [ordered-set "0.2.2"]])
+                 [ordered-set "0.2.2"]]
+  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.1"]]
+  :tasks [cake-marginalia.tasks])
