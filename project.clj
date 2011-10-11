b3b17ab692574553606a7e28056884ffd001a028
Brian Marick <marick@exampler.com>
2011-10-11 13:53:29 -0500

    Changes to run under both 1.3 and 1.2.1

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject ordered "0.3.0"
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
-  :dependencies [[clojure "1.2.1"]
-                 [clojure-contrib "1.2.0"]
-                 [ordered-set "0.2.2"]]
-  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.1"]]
+  :dependencies [[clojure "[1.2.1,1.3.0]"]
+                  ; [clojure-contrib "1.2.0"] ; Not used anywhere.
+                 ]
+  :dev-dependencies [; [org.clojars.flatland/cake-marginalia "0.6.1"]
+                     ; This dependency doesn't download.
+                     [ordered-set "0.2.2"]] ; Not required except for benchmarks.
   :tasks [cake-marginalia.tasks])
