29960de4ab0471076728ee4846fdd6dbd2b61344
Alan Malloy <alan@malloys.org>
2012-04-04 13:51:45 -0700

    Don't be so picky about versions of Clojure.
    If we depend on [1.2.1,1.3.0], then it's impossible to even TRY to run
    it on 1.4, but it should work fine for many future Clojure versions.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject ordered "1.1.0"
+(defproject ordered "1.1.1"
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
-  :dependencies [[clojure "[1.2.1,1.3.0]"]]
-  :dev-dependencies [[ordered-set "0.2.2"] ; Not required except for benchmarks.
-                     ]
+  :dependencies [[clojure "1.2.1"]]
+  :dev-dependencies [[ordered-set "0.2.2"]] ; Not required except for benchmarks.
   :tasks [cake-marginalia.tasks])
