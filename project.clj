b72639a31a039aa7bd1cd3958af365211feb53d8
Justin Balthrop <git@justinbalthrop.com>
2012-05-01 18:26:22 -0700

    bump dep version

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
   :dependencies [[clojure "1.4.0"]]
   ;; Not required except for benchmarks.
-  :dev-dependencies [[ordered-collections "0.4.1"]])
+  :dev-dependencies [[ordered-collections "0.4.2"]])
