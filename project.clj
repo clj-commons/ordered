8aef935d3a1fa56318ce13ccd1f9c527bc889c5c
Alan Malloy <alan@malloys.org>
2012-06-13 10:57:10 -0700

    Version 1.3.0
    Update project.clj to support lein2, and fix a bug in map/conj.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject ordered "1.2.2"
+(defproject ordered "1.3.0"
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
-  :dependencies [[clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   ;; Not required except for benchmarks.
   :dev-dependencies [[ordered-collections "0.4.2"]])
