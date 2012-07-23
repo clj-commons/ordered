7c4a263d30926420f57f458320bcc97bbbc60657
Anthony Grimes <i@raynes.me>
2012-07-23 16:01:48 -0500

    Add :url and :license keys.

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject ordered "1.3.0"
+  :url "https://github.com/flatland/ordered"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
   :dependencies [[org.clojure/clojure "1.4.0"]]
   ;; Not required except for benchmarks.
