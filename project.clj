a72614f6413173e12a02db615d1a422e76adebb0
Anthony Grimes <i@raynes.me>
2012-07-23 16:03:42 -0500

    Move to lein2 for real.
    As opposed to whatever amalloy thought moving to lein2 entailed.

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
   :dependencies [[org.clojure/clojure "1.4.0"]]
   ;; Not required except for benchmarks.
-  :dev-dependencies [[ordered-collections "0.4.2"]])
+  :profiles {:dev {:dependencies [[ordered-collections "0.4.2"]]}})
