77e51e595110302b289de4e0f7888eb4405eca2a
Ben Mabey <ben@benmabey.com>
2012-09-06 15:00:03 -0600

    AOTCs the ordered types to allow for top-level use
    See this gist to understand why this is needed:
     https://gist.github.com/3660304

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
   :dependencies [[org.clojure/clojure "1.4.0"]]
+  :aot [ordered.map ordered.set]
   ;; Not required except for benchmarks.
   :profiles {:dev {:dependencies [[ordered-collections "0.4.2"]]}})
