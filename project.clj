8c11e035b7adab65985b9ff5976afd27b6010870
Justin Balthrop <git@justinbalthrop.com>
2012-11-16 11:24:38 -0800

    1.4.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.flatland/ordered "1.3.3-SNAPSHOT"
+(defproject org.flatland/ordered "1.4.0"
   :url "https://github.com/flatland/ordered"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.flatland/useful "0.8.9-SNAPSHOT"]]
+                 [org.flatland/useful "0.9.0"]]
   ;; Not required except for benchmarks.
   :profiles {:dev {:dependencies [[ordered-collections "0.4.2"]]}})
