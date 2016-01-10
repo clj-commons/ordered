0bd618329001213774f1e21e399751c01185ee49
eyelidlessness <gnosis@gmail.com>
2016-01-10 07:39:38 -0800

    ClojureScript implementation of OrderedSet
    Updated tests where necessary to exercise both implementations

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Pure Clojure/ClojureScript implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
                  [org.flatland/useful "0.9.0"]]
   ;;:aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.4,default:dev,1.5,default:dev,1.7,default" "test"]}
   :aliases {"testall" ["with-profile" "dev,default:dev,1.4,default:dev,1.5,default:dev,1.7,default:dev,1.7,cljs,default" "test"]
