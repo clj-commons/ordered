d7fc5662093448ababede46339f7f0cf0c03754d
Alan Malloy <alan@malloys.org>
2016-07-14 17:38:49 -0700

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/ordered "1.5.3"
+(defproject org.flatland/ordered "1.5.4-SNAPSHOT"
   :url "https://github.com/amalloy/ordered"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -7,7 +7,7 @@
                  [org.flatland/useful "0.9.0"]]
   ;;:aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.4,default:dev,1.5,default:dev,1.7,default" "test"]}
   :aliases {"testall" ["with-profile" "dev,default:dev,1.4,default:dev,1.5,default:dev,1.7,default" "test"]}
-  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-RC1"]]}
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
