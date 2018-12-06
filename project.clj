e39579f73e7087d1a0c7bd9af66653a22b037691
Daniel Compton <desk@danielcompton.net>
2018-12-07 09:41:50 +1300

    Bump org.flatland/useful to 0.11.6

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.flatland/useful "0.9.0"]]
+                 [org.flatland/useful "0.11.6"]]
   :aliases {"testall" ["with-profile" "dev,default:dev,1.6,default:dev,1.7,default:dev,1.8,default:dev,1.10,default" "test"]}
   :profiles {:1.10 {:dependencies [[org.clojure/clojure "1.10.0-RC3"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
