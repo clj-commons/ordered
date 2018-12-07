1f989547d1900204b848d1aed040ab210ba5027e
Daniel Compton <desk@danielcompton.net>
2018-12-07 15:57:06 +1300

    Simplify test aliases

--- project.clj
+++ project.clj
@@ -5,10 +5,11 @@
   :description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.flatland/useful "0.11.6"]]
-  :aliases {"testall" ["with-profile" "dev,default:dev,1.6,default:dev,1.7,default:dev,1.8,default:dev,1.10,default" "test"]}
+  :aliases {"testall" ["with-profile" "+1.6:+1.7:+1.8:+1.9:+1.10" "test"]}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :profiles {:1.10 {:dependencies [[org.clojure/clojure "1.10.0-RC3"]]}
+             :1.9  {}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
