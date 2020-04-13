e5c16dc1810624fe743025ae3e3c9b6a1615bc47
Daniel Compton <desk@danielcompton.net>
2020-04-13 20:13:30 +1200

    Version 1.5.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/ordered "1.5.8"
+(defproject org.flatland/ordered "1.5.9-SNAPSHOT"
   :url "https://github.com/clj-commons/ordered"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
