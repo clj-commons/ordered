19c1ddba234b2947f05075dca79d1517e93746e1
Lee Read <lee@dlread.com>
2026-09-03 17:59:13 -0400

    bb tasks: add lint-eastwood (#91)
    * bb tasks: add lint-eastwood
    
    Also add docs & ci.
    
    There are some lint findings, we'll address those next.
    
    Contributes to #72 and #82
    
    * address eastwood finding: maybe don't use reduce
    
    Finding was:
    ```
    src/flatland/ordered/set.clj:83:5: unused-ret-vals {:kind :invoke}:
    Return value is discarded for a function call that only has side effects
    if the functions passed to it as args have side effects: (reduce (fn
    [idx item] (aset dest idx item) (inc idx)) 0 (.seq this)).
    ```
    
    I think eastwood has a point here.
    Clearer to use a loop here, I think.
    
    m# Please enter the commit message for your changes. Lines starting
    
    * address eastwood findings: ignore test code
    
    There were a bunch of uninteresting eastwood findings on our test code.
    Let's not run eastwood on our test code.
    
    * docs: changelog

---
+++ script/lint_eastwood.clj
@@ -0,0 +1,11 @@
+(ns lint-eastwood
+  (:require [helper.shell :as shell]
+            [lread.status-line :as status]))
+
+(defn lint []
+  (status/line :head "eastwood: linting")
+  (shell/command "clojure -M:test-common:eastwood"))
+
+(defn task
+  [_opts]
+  (lint))
