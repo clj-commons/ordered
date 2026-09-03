4752830707e561ca35f24fe7f7f4e5f20654e743
lread <lee@dlread.com>
2026-09-03 16:19:34 -0400

    bb tasks: add lint-kondo
    Also add docs & ci.
    
    There are some lint findings, we'll address those next.
    
    Contributes to #72 and #82

---
+++ script/lint_kondo.clj
@@ -0,0 +1,64 @@
+(ns lint-kondo
+  (:require [babashka.fs :as fs]
+            [clojure.string :as string]
+            [helper.shell :as shell]
+            [lread.status-line :as status]))
+
+(def clj-kondo-cache ".clj-kondo/.cache")
+
+(defn- cache-exists? []
+  (fs/exists? clj-kondo-cache))
+
+(defn- delete-cache []
+  (when (cache-exists?)
+    (fs/delete-tree clj-kondo-cache)))
+
+(defn- build-cache []
+  (when (cache-exists?)
+    (delete-cache))
+  (let [clj-cp (-> (shell/clojure {:out :string}
+                                  "-Spath -M:test")
+                   with-out-str
+                   string/trim)
+        bb-cp (-> (shell/command {:out :string}
+                                 "bb print-deps --format classpath")
+                  :out
+                  string/trim)]
+    (status/line :detail "- copying lib configs and creating cache")
+    (shell/command "clojure -M:clj-kondo --skip-lint --copy-configs --dependencies --lint" clj-cp bb-cp)))
+
+(defn- check-cache [{:keys [rebuild]}]
+  (status/line :head "clj-kondo: cache check")
+  (if-let [rebuild-reason (cond
+                            rebuild
+                            "Rebuild requested"
+
+                            (not (cache-exists?))
+                            "Cache not found"
+
+                            :else
+                            (let [updated-dep-files (fs/modified-since clj-kondo-cache ["deps.edn" "bb.edn"])]
+                              (when (seq updated-dep-files)
+                                (format "Found deps files newer than lint cache: %s" (mapv str updated-dep-files)))))]
+    (do (status/line :detail rebuild-reason)
+        (build-cache))
+    (status/line :detail "Using existing cache")))
+
+(defn- lint [opts]
+  (check-cache opts)
+  (status/line :head "clj-kondo: linting")
+  (let [{:keys [exit]}
+        (shell/command {:continue true}
+                       "clojure -M:clj-kondo --parallel --lint src test script deps.edn")]
+    (cond
+      (= 2 exit) (status/die exit "clj-kondo found one or more lint errors")
+      (= 3 exit) (status/die exit "clj-kondo found one or more lint warnings")
+      (> exit 0) (status/die exit "clj-kondo returned unexpected exit code"))))
+
+(def cli-opts {:spec {:rebuild {:coerce :boolean
+                                :desc "Force rebuild of clj-kondo lint cache"}}})
+
+(defn task
+  {:org.babashka/cli cli-opts}
+  [opts]
+  (lint opts))
