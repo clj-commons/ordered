8c32450e09a3f424e92dccc307b7dd0fb19842c7
lread <lee@dlread.com>
2026-09-04 10:07:51 -0400

    bb tasks: add pubcheck & publish
    This change migrates from lein/circleci to deps.edn/github actions for publish.
    
    Publish is now automated via `bb publish`.
    It automatically updates version, updates changelog Unreleased section, and
    version reference in User Guide.
    
    Some differences:
    - GitHub Release is auto-created with a link to the changelog
    - We use babashka neil version conventions to track/bump version in `deps.edn`
    - No longer signing jar - value is questionable, setup is complex,
    making it a high maintenance burden
    - Git version tag is now more conventional v1.2.3 instead of Release-1.2.3
    
    Publish flow is described in Maintainers Guide.
    
    Misc:
    - Added "Audience" section to each guide
    - Changelog now thanks contributor for their change
    - Changelog describes version scheme a bit more Clojurely
    - README now lists current active maintainers
    
    Retired:
    - old circleci config
    - old kaocha remnants
    - old lein project.clj
    
    Closes #72
    Closes #82
    Closes #83

--- build.clj
+++ build.clj
@@ -1,6 +1,65 @@
 (ns build
-  (:require [clojure.edn :as edn]
+  (:require [build-shared]
+            [clojure.edn :as edn]
             [clojure.tools.build.api :as b]))
+
+(def version (build-shared/lib-version))
+(def lib (build-shared/lib-artifact-name))
+
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s.jar" (name lib)))
+
+(defn jar
+  "Build library jar file.
+  Supports `:version-override` for local testing, otherwise official version is used.
+  For example, when testing 3rd party libs against rewrite-clj HEAD we use the suffix: canary."
+  [{:keys [version-override] :as opts}]
+  (b/delete {:path class-dir})
+  (b/delete {:path jar-file})
+  (let [version (or version-override version)]
+    (println "jarring version" version)
+    (b/write-pom {:class-dir class-dir
+                  :lib lib
+                  :version version
+                  :basis basis
+                  :src-dirs ["src"]
+                  :scm {:url "https://github.com/clj-commons/ordered"
+                        :connection "scm:git:git@github.com:clj-commons/ordered.git"
+                        :developerConnection "scm:git:git@github.com:clj-commons/ordered.git"
+                        :tag (format "v%s" version)}
+                  :pom-data [[:description "Pure-clojure implementation of ruby's ordered hash and set types - instead of sorting by key, these collections retain insertion order."]
+                             [:url "https://github.com/clj-commons/ordered"]
+                             [:licenses
+                              [:license
+                               [:name "Eclipse Public License - v 1.0"]
+                               [:url "http://www.eclipse.org/legal/epl-v10.html"]]]
+                             [:properties
+                              [:project.build.sourceEncoding "UTF-8"]]]})
+    (b/copy-dir {:src-dirs ["src" "resources"]
+                 :target-dir class-dir})
+    (b/jar {:class-dir class-dir
+            :jar-file jar-file})
+    (assoc opts :built-version version)))
+
+(defn install
+  "Install built jar to local maven repo, optionally specify `:version-override` for local testing."
+  [opts]
+  (let [{:keys [built-version]} (jar opts)]
+    (println "installing version" built-version)
+    (b/install {:class-dir class-dir
+                :lib lib
+                :version built-version
+                :basis basis
+                :jar-file jar-file})))
+
+(defn deploy [_]
+  (jar {})
+  (println "deploy")
+  ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+   {:installer :remote
+    :artifact jar-file
+    :pom-file (b/pom-path {:lib lib :class-dir class-dir})}))
 
 (defn download-deps
   "Download all deps for all aliases"
