0d46c0519352cbdecc46eb0cd1628de10f0d2832
Lee Read <lee@dlread.com>
2026-09-04 08:24:12 -0400

    bb tasks: add dev-jvm & dev-cljs (#94)
    * bb tasks: add dev-jvm & dev-cljs
    
    There are some warnings emitted on bb dev-cljs startup, like:
    ```
    WARNING: Middleware #'cider.piggieback/wrap-cljs-repl is required by #'cider.nrepl/wrap-complete but is not present in middleware list.
    ```
    But they don't seem to have a negative effect.
    
    Contributes to #82
    
    * doc: describe bb repl tasks

---
+++ script/dev_repl.clj
@@ -0,0 +1,43 @@
+(ns dev-repl
+  (:require [babashka.process :as process]
+            [clojure.string :as str]
+            [helper.clojure-versions :as clojure-versions]
+            [lread.status-line :as status]))
+
+(defn- launch-repl [flavor {:keys [flowstorm host bind port]}]
+  (let [aliases (cond-> [(case flavor
+                           :cljs "nrepl/cljs:cljs"
+                           :jvm  "nrepl/jvm")]
+                  flowstorm (conj "flowstorm"))]
+    (status/line :head "Launching Clojure %s nREPL" (name flavor))
+    (when flowstorm
+      (status/line :detail "Flowstorm support is enabled"))
+    (process/exec "clj" (str "-M:" (:alias (clojure-versions/current-prod)) ":test-common:nrepl:" (str/join ":" aliases))
+                  "-h" host
+                  "-b" bind
+                  "-p" port)))
+
+(def cli-repl-opts {:spec {:host {:ref "<ADDR>"
+                                  :alias :h
+                                  :default "127.0.0.1"
+                                  :desc "Host address"}
+                           :bind {:ref "<ADDR>"
+                                  :alias :b
+                                  :default "127.0.0.1"
+                                  :desc "Bind address"}
+                           :port {:ref "<symbols>"
+                                  :coerce :int
+                                  :default 0
+                                  :alias :p
+                                  :desc "Port, 0 for auto-select"}}})
+
+;; Entry points
+(defn dev-jvm
+  {:org.babashka/cli cli-repl-opts}
+  [opts]
+  (launch-repl :jvm opts))
+
+(defn dev-cljs
+  {:org.babashka/cli cli-repl-opts}
+  [opts]
+  (launch-repl :cljs opts))
