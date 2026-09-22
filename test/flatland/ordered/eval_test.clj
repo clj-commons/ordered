40c678ae894a20faca88c935b052b9696c219b81
Lee Read <lee@dlread.com>
2026-09-22 14:50:02 -0400

    test: add simple reporting (#112)
    * test: add simple reporting
    
    It reassures me to see what tests were run with platform info.
    
    * test report: simply use println for cljs too

--- test/flatland/ordered/eval_test.clj
+++ test/flatland/ordered/eval_test.clj
@@ -2,7 +2,8 @@
   "These tests are in this namespace to make them easy to skip when running tests under GraalVM.
   The make use of `eval` which is not a thing we can to in a GraalVM native image.
   I assume they are important, they were created in response to https://github.com/clj-commons/ordered/issues/55   "
-  (:require [clojure.test :refer [deftest is]]))
+  (:require [clojure.test :refer [deftest is]]
+            [flatland.ordered.test-report]))
 
 (deftest map-print-read-eval-ordered
   (is (= (pr-str (eval (read-string "#ordered/map[[:a 1] [:b 2]]")))
